6a250cde4d1e0b562332f077ccc7efb1b2b1ce56
SebastianGonzalez216 <psi.ragnar@gmail.com>
2023-07-12 12:32:39 -0600

    DTOs:CreateResponseAndRequest

---
+++ DTO/AuthResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace be_scrapping;
+
+    public class AuthResponse
+    {  
+        public required string Email;
+
+        public required string UserName;
+
+        public required string Token;
+    }
+
